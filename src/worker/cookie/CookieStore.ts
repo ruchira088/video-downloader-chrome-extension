5f286ab20a6e01f060cecd96bba754b25e7f917a
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-12-10 11:40:00 +1100

    Adding getAll method for CookieStore

--- src/worker/cookie/CookieStore.ts
+++ src/worker/cookie/CookieStore.ts
@@ -3,6 +3,8 @@ import Cookie = chrome.cookies.Cookie
 
 export interface CookieStore {
   get(key: string): Promise<Maybe<Cookie>>
+
+  getAll(): Promise<Cookie[]>
 }
 
 class ChromeCookieStore implements CookieStore {
@@ -10,9 +12,14 @@ class ChromeCookieStore implements CookieStore {
   }
 
   get(key: string): Promise<Maybe<Cookie>> {
+    return this.getAll()
+      .then(cookies => Maybe.fromFalsy(cookies.find((cookie) => cookie.name === key)))
+  }
+
+  getAll(): Promise<Cookie[]> {
     return new Promise((resolve) =>
-      chrome.cookies.getAll({ url: this.url }, (result) =>
-        resolve(Maybe.fromFalsy(result.find((cookie) => cookie.name === key)))
+      chrome.cookies.getAll({ url: this.url }, cookies =>
+        resolve(cookies)
       )
     )
   }
